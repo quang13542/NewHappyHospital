a01ad1bbfe7f8523d0bf4fcf5d0a8cd21e2a600d
mungvt <vutienmung@gmail.com>
2022-07-03 06:51:39 +0700

    feat: setting broad

--- src/HappyHospital/GameFrame.java
+++ src/HappyHospital/GameFrame.java
@@ -1,12 +1,32 @@
 package HappyHospital;
 
+import java.awt.BorderLayout;
+import java.awt.TextField;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import HappyHospital.GamePanel;
 
 public class GameFrame extends JFrame{
 
 	GameFrame(){
+		JButton saveBotton = new JButton("Save Data");
+		JButton loadBotton = new JButton("Load Data");
+		JButton applyBotton = new JButton("Apply");
+		JLabel dealineLabel = new JLabel("AGV Deadline");
+		TextField numberAgentsTextFiled = new TextField("Number of agents.");
+		TextField deadlineTextFiled = new TextField("DES_47: 00:01:25 ± 4");
+		
+		SettingPanel settingPanel = new SettingPanel();
+		settingPanel.add(saveBotton);
+		settingPanel.add(loadBotton);
+		settingPanel.add(numberAgentsTextFiled);
+		settingPanel.add(applyBotton);
+		settingPanel.add(dealineLabel, BorderLayout.EAST);
+		settingPanel.add(deadlineTextFiled);
+		this.add(settingPanel, BorderLayout.SOUTH);
 		
 		this.add(new GamePanel());
 		this.setTitle("Happy Hospital");
